2d190c72bfe160512a06fa4e328699e2a4bcebc3
MayankPandey19136016 <mayankpandeyji2001@gmail.com>
2026-02-14 12:21:13 +0530

    utils code setup

--- src/index.js
+++ src/index.js
@@ -2,4 +2,16 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 dotenv.config();
 
-connectDB();
+connectDB()
+  .then(() => {
+    const server = app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port : ${process.env.PORT}`);
+    });
+    server.on("error", (error) => {
+      console.error("Server failed to start", error);
+      process.exit(1);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGODB Connection failed !!!!", error);
+  });
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// const asyncHandler = () => { }
+// const asyncHandler = (func) => { }
+// const asyncHandler = (function) => async() =>{}
+
+// try catch approch for async wrapper
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
